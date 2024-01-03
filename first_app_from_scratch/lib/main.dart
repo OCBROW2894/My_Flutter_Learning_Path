30ed3357df09046db5cf465142dcd7badf09c4c9
OCBROW2894 <ochiengbryianowuor2894@gmail.com>
2024-01-03 20:20:59 +0300

    Grass

--- first_app_from_scratch/lib/main.dart
+++ first_app_from_scratch/lib/main.dart
@@ -1,11 +1,16 @@
-import "package:flutter/material.dart";
+import 'package:flutter/material.dart';
 
-void main(){
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  @override
   Widget build (BuildContext context){
-    return MaterialApp(home: Text("Hello Bryian Welcome to Flutter"),);
+    return  MaterialApp(home: Scaffold(
+      appBar: AppBar(
+        title: Text("My First Flutter App"),
+      ),
+      body: Text("Bryian how Do you feel"),
+    ),
+    );
   }
 }
